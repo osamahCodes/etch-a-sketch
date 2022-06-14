652ce34bcc278bf868d23f65b27d76ff208a4572
osamahCodes <osamahalghonaim@gmail.com>
2022-06-14 12:20:53 +0300

    Add coloring brush and ratio to UI and JS

--- javascript.js
+++ javascript.js
@@ -1,16 +1,32 @@
 let canvas = document.querySelector('.canvas');
 let options = document.querySelector('.options');
+let current = document.querySelector('.current');
+let randomizer = document.querySelector('.randomizer');
+let isBrushBlack = true;
 for (let i = 0; i < 64*64; i++) {
     const div = document.createElement('div');
-    div.style.cssText = 'border-color: dimgray; border-width: 1px; border-style: solid; height: 6.25px; width: 6.25px;';
-    div.classList.add('dots')
+    div.style.cssText = 'border-color: white; border-width: 1px; border-style: solid; height: 6.25px; width: 6.25px;';
+    div.classList.add('dots');
     canvas.appendChild(div);
     
 }
 
+function brush(boolean){
+    if(boolean){
+        return 'black';
+    }else{
+        let r = Math.floor(Math.random()*255)+1;
+        let g = Math.floor(Math.random()*255)+1;
+        let b = Math.floor(Math.random()*255)+1;
+        return `rgb(${r},${g},${b})`;
+    }
+}
+
 canvas.addEventListener('mouseover', function(e){
     if (e.target.classList.value==='dots') {
-        e.target.style['background-color'] = 'black';
+        let color = brush(isBrushBlack);
+        e.target.style['background-color'] = color;
+        e.target.style['border-color'] = color;
     }
 }, false)
 options.addEventListener('click', function(e){
@@ -25,11 +41,16 @@ options.addEventListener('click', function(e){
         canvas.replaceChildren();
         for (let i = 0; i < newRatio*newRatio; i++) {
             const div = document.createElement('div');
-            div.style.cssText = `border-color: dimgray; border-width: 1px; border-style: solid; height: ${newPixel}px; width: ${newPixel}px;`;
+            div.style.cssText = `border-color: white; border-width: 1px; border-style: solid; height: ${newPixel}px; width: ${newPixel}px;`;
+            current.textContent = `Current ratio: ${newRatio}X${newRatio}`
             div.classList.add('dots')
             canvas.appendChild(div);
             
         }
         
-    }
+    }else if (e.target.classList.value==='randomizer') {
+        isBrushBlack = false;
+    }else if (e.target.classList.value==='black') {
+        isBrushBlack = true;
+    } 
 }, false)
